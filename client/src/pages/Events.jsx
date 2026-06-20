d548e78e54da2ffb592d3c521878a6e3aabd67aa
ekilroy2028 <ekilroy1@asu.edu>
2026-06-20 12:13:03 -0400

    add Stress 2

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import Event from '../components/Event'
 import EventsAPI from '../services/EventsAPI'
+import LocationsAPI from '../services/LocationsAPI'
 import '../css/LocationEvents.css'
 
 const Events = () => {
     const [events, setEvents] = useState([])
+    const [locations, setLocations] = useState([])
+    const [selectedLocation, setSelectedLocation] = useState('all')
 
     useEffect(() => {
         (async () => {
             try {
                 const eventsData = await EventsAPI.getAllEvents()
                 setEvents(eventsData)
+
+                const locationsData = await LocationsAPI.getAllLocations()
+                setLocations(locationsData)
             }
             catch (error) {
                 console.error('Error loading events:', error)
@@ -18,11 +24,31 @@ const Events = () => {
         })()
     }, [])
 
+    const filteredEvents = selectedLocation === 'all'
+        ? events
+        : events.filter(event => event.location_id === parseInt(selectedLocation))
+
     return (
         <div className='location-events'>
+            <div style={{ textAlign: 'center', margin: '20px 0' }}>
+                <label htmlFor='locationFilter' style={{ marginRight: '10px', fontWeight: 'bold' }}>
+                    Filter by location:
+                </label>
+                <select
+                    id='locationFilter'
+                    value={selectedLocation}
+                    onChange={(e) => setSelectedLocation(e.target.value)}
+                >
+                    <option value='all'>All Locations</option>
+                    {locations.map(location =>
+                        <option key={location.id} value={location.id}>{location.name}</option>
+                    )}
+                </select>
+            </div>
+
             <main>
                 {
-                    events && events.length > 0 ? events.map((event) =>
+                    filteredEvents && filteredEvents.length > 0 ? filteredEvents.map((event) =>
                         <Event
                             key={event.id}
                             id={event.id}
